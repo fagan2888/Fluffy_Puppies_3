cd20f94bfd9e882c08f44a02266e4b01be2bfde5
Alex Damiani <alexdmail15@yahoo.com>
2016-09-18 18:27:52 -0700

    Fixed bug with buyer payment sum

--- Q4.py
+++ Q4.py
@@ -20,6 +20,10 @@ def Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment,
 
 	M_total_seller_payment = 0
 
+	arr = []
+	arr2 = []
+	arr3 = []
+
 	for t in range(num_periods):
 		M_missing_principal = 0 	# --> assumption: principal is never guaranteed 
 		M_principal_payment = M_CF_principal[t]
@@ -36,7 +40,11 @@ def Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment,
 			print ('Principal goes negative at time step t = ', t)
 			break
 
-		M_total_buyer_payment += ((M_principal) * M_coupon * np.exp(-rf_arr[t]))
+		M_total_buyer_payment += (M_principal * M_coupon * np.exp(-rf_arr[t]))
+
+		arr.append(M_principal * M_coupon * np.exp(-rf_arr[t]))
+		arr2.append(M_coupon)
+		arr3.append(np.exp(-rf_arr[t]))
 
 		# seller pays out (1 - recovery rate) * defaulted principal
 		# sellers pays out 100% of interest and principal shortfalls
@@ -106,6 +114,12 @@ for i in [4, 7]:		# 4 = M2;  7 = M5
 		M_seller_payments = []
 
 		for k in range(num_sims):
+			### reset total buuyer payment each path to initial premium
+			if i == 4:
+				M_total_buyer_payment = M2_total_buyer_payment
+			elif i == 7:
+				M_total_buyer_payment = M5_total_buyer_payment
+
 			M_total_seller_payment, M_total_buyer_payment = \
 				Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment, M_CF_principal[k], 
 					M_CF_prepay[k], M_CF_default[k], M_CF_neg_int[k], ir.r_matrix[k])
@@ -123,10 +137,11 @@ for i in [4, 7]:		# 4 = M2;  7 = M5
 		M5_avg_buyer_payment = (np.mean(M_buyer_array[0]) + np.mean(M_buyer_array[1])) / 2.0
 		M5_avg_seller_payment = (np.mean(M_seller_array[0]) + np.mean(M_seller_array[1])) / 2.0
 
-print (M2_avg_buyer_payment, M2_avg_buyer_payment)
+
+print (M2_avg_buyer_payment, M2_avg_seller_payment)
 print (M5_avg_buyer_payment, M5_avg_seller_payment)
 
-Check_Correct_Position(M2_avg_buyer_payment, M2_avg_buyer_payment, "M2")
+Check_Correct_Position(M2_avg_buyer_payment, M2_avg_seller_payment, "M2")
 Check_Correct_Position(M5_avg_buyer_payment, M5_avg_seller_payment, "M5")
 
 """
