510a7c2ed20538cd124fa8f79dbe3f8e43a4de8f
Alex Damiani <alexdmail15@yahoo.com>
2016-09-18 17:12:55 -0700

    Removed unnecessary code

--- Q4.py
+++ Q4.py
@@ -11,17 +11,14 @@ import interest_rates as ir
 		M2_coupon * number of period, M5_coupon * number of periods
 """
 
-def Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment, num_periods, 
+def Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment,
 				M_CF_principal, M_CF_prepay, M_CF_default, M_CF_neg_int, rf_arr):
 	"""
 		Do for both M2 and M5.
 		Payments for seller and buyer made at end of each month.
 	"""
-	M_principal_payment = M_missing_interest = M_missing_principal = \
-	M_total_seller_payment = M_defaulted_principal = M_prepayment = 0
-
 	for t in range(num_periods):
-		M_missing_principal = 0 	# --> principal is never guaranteed 
+		M_missing_principal = 0 	# --> assumption: principal is never guaranteed 
 		M_principal_payment = M_CF_principal[t]
 		M_missing_interest = M_CF_neg_int[t]
 		M_defaulted_principal = M_CF_default[t]
@@ -33,18 +30,24 @@ def Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment, num_periods,
 		M_principal -= (M_principal_payment + M_prepayment + M_defaulted_principal)
 
 		if M_principal <= 0:
-			print ('negative at t = ', t)
+			print ('Principal goes negative at time step t = ', t)
 			break
 
 		M_total_buyer_payment += ((M_principal) * M_coupon * np.exp(-rf_arr[t]))
 
-		# seller's formula simplified, need to check with TA to line up with formula
 		# seller pays out (1 - recovery rate) * defaulted principal
 		# sellers pays out 100% of interest and principal shortfalls
 		M_total_seller_payment += (M_defaulted_principal * (1 - recovery_rate)) *  np.exp(-rf_arr[t])
 		M_total_seller_payment += (M_missing_principal + M_missing_interest) *  np.exp(-rf_arr[t])
 
 	return M_total_seller_payment, M_total_buyer_payment
+
+def Check_Correct_Position(M_buyer_payment, M_seller_payment, tranche):
+		print ("For the " + tranche + " tranche, want to be in the position of CDS ", end=' ')
+		if M_avg_buyer_payment > M_avg_seller_payment:
+			print ("buyer")
+		else:
+			print ("seller")
 
 ### All values given in assignment
 M2_principal = 30150000.0
@@ -66,27 +69,13 @@ recovery_rate = 0.40
 M2_total_buyer_payment = M2_premium # initial amount as upfront premium
 M5_total_buyer_payment = M5_premium 
 
-M2_CF_total = wf.CF_total[4,:,:]
-M2_CF_principal = wf.CF_principal[4]
-M2_CF_prepay = wf.CF_prepay[4]
-M2_CF_default = wf.CF_default[4]
-M2_CF_interest = wf.CF_interest[4]
-M2_CF_neg_int = wf.CF_neg_int[4]
-
-M5_CF_total = wf.CF_total[7]
-M5_CF_principal = wf.CF_principal[7]
-M5_CF_prepay = wf.CF_prepay[7]
-M5_CF_default = wf.CF_default[7]
-M5_CF_interest = wf.CF_interest[7]
-M5_CF_neg_int = wf.CF_neg_int[7]
-
-num_sims = M2_CF_total.shape[0]
-num_periods = M2_CF_total.shape[1]
+num_sims = ir.num_sims
+num_periods = 315
 
 r_matrix_arr = [ir.r_matrix, ir.r_anti_matrix]
 
 
-for i in [4, 7]:
+for i in [4, 7]:		# 4 = M2;  7 = M5
 	M_seller_array = []
 	M_buyer_array = []
 
@@ -115,9 +104,8 @@ for i in [4, 7]:
 
 		for k in range(num_sims):
 			M_total_seller_payment, M_total_buyer_payment = \
-				Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment, num_periods, 
-							M_CF_principal[k], M_CF_prepay[k], M_CF_default[k], M_CF_neg_int[k], 
-							ir.r_matrix[k])
+				Calc_CDS_Legs(M_principal, M_coupon, M_total_buyer_payment, M_CF_principal[k], 
+					M_CF_prepay[k], M_CF_default[k], M_CF_neg_int[k], ir.r_matrix[k])
 
 			M_buyer_payments.append(M_total_buyer_payment)
 			M_seller_payments.append(M_total_seller_payment)
@@ -132,16 +120,11 @@ for i in [4, 7]:
 		M5_avg_buyer_payment = (np.mean(M_buyer_array[0]) + np.mean(M_buyer_array[1])) / 2.0
 		M5_avg_seller_payment = (np.mean(M_seller_array[0]) + np.mean(M_seller_array[1])) / 2.0
 
-print (M2_avg_buyer_payment, M2_avg_seller_payment)
+print (M2_avg_buyer_payment, M2_avg_buyer_payment)
 print (M5_avg_buyer_payment, M5_avg_seller_payment)
 
-'''
-M5_total_seller_payment, M5_total_buyer_payment= \
-	Calc_CDS_Legs(M5_principal, M5_coupon, M5_total_buyer_payment, num_periods)
-
-### Calculate final value as difference of seller and buyer payments
-M2_CDS_value = M2_total_seller_payment - M2_total_buyer_payment
-M5_CDS_value = M5_total_seller_payment - M5_total_buyer_payment'''
+Check_Correct_Position(M2_avg_buyer_payment, M2_avg_buyer_payment, tranche)
+Check_Correct_Position(M5_avg_buyer_payment, M5_avg_seller_payment, tranche)
 
 """
 	interpret results as which position is more profitable. 
